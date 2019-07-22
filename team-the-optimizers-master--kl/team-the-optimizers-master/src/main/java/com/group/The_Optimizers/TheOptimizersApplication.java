883a92085ac8c122b558692d7aa68368144d05bb
Kayla Luna <46774169+knluna@users.noreply.github.com>
2019-07-21 18:09:26 -0600

    Add files via upload

---
+++ team-the-optimizers-master--kl/team-the-optimizers-master/src/main/java/com/group/The_Optimizers/TheOptimizersApplication.java
@@ -0,0 +1,13 @@
+package com.group.The_Optimizers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOptimizersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOptimizersApplication.class, args);
+	}
+
+}
