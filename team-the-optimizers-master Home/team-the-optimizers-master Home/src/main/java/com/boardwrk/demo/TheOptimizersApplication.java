adf1a2c8b05215e601ae94bfcc396839391bcc09
Kayla Luna <46774169+knluna@users.noreply.github.com>
2019-08-04 21:54:30 -0600

    Add files via upload
    Updated HP--8.4

---
+++ team-the-optimizers-master Home/team-the-optimizers-master Home/src/main/java/com/boardwrk/demo/TheOptimizersApplication.java
@@ -0,0 +1,12 @@
+package com.boardwrk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOptimizersApplication {
+    public static void main(String[] args) {
+        // launch the application
+        SpringApplication.run(TheOptimizersApplication.class, args);
+    }
+}
